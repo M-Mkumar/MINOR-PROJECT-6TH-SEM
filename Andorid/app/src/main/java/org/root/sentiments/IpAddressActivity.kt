611198e620027e13a7f66a22a24de135cc05ff72
this-Aditya <adityamishra2217@gmail.com>
2024-03-23 22:58:19 +0530

    Added setup for activities and drawables

---
+++ Andorid/app/src/main/java/org/root/sentiments/IpAddressActivity.kt
@@ -0,0 +1,11 @@
+package org.root.sentiments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IpAddressActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ip_address)
+    }
+}
